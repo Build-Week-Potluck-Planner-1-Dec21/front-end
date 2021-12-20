9d29944387b4cb1770a345be04dcbdfdd81b0c4f
Peter Conley <peterdavidconley@gmail.com>
2021-12-20 14:52:57 -0500

    imported all necessary components to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+// Importing 
+import CreateAccount from './Components/CreateAccount';
+import LogOut from './Components/Logout';
+import CreatePotluck from './Components/CreatePotluck';
+import EventRSVP from './Components/EventRSVP';
+import Home from './Components/Home';
+import NavBar from './Components/NavBar'
+import ViewEvents from './Components/ViewEvents'
+import Login from './Components/Login';
+
+// Routing
+import { Route } from 'react-router-dom';
+
 
 function App() {
   return (
---
+++ src/Components/Logout.js
@@ -0,0 +1,18 @@
+// Imports here
+import React from 'react'
+
+
+//Component
+
+const Logout = () => {
+
+    return(
+        <div>
+
+        </div>
+    )
+};
+
+//Export
+
+export default Logout;

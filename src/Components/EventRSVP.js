f75e070ba40cbe868228522702dc93d565db096d
Peter Conley <peterdavidconley@gmail.com>
2021-12-20 16:11:54 -0500

    created form for EventRSVP

--- src/Components/EventRSVP.js
+++ src/Components/EventRSVP.js
@@ -7,6 +7,21 @@ const EventRSVP = () => {
 
     return(
         <div>
+            <form>
+                <label>Name:
+                    <input />
+                </label>
+                <label>Item 1:
+                    <input type='text'/>
+                </label>
+                <label>Item 2:
+                    <input type='text'/>
+                </label>
+                <label>Item 3:
+                    <input type='text'/>
+                </label>
+                <button>Submit</button>
+            </form>
 
         </div>
     )

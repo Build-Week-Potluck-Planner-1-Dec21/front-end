ed5929827850f52bdcf5640567c9bf63417dbdec
Peter Conley <peterdavidconley@gmail.com>
2021-12-20 14:59:38 -0500

    imported styled components and wrapped index.js in Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import React from 'react';
 
-// Importing 
+// Importing Components
+
 import CreateAccount from './Components/CreateAccount';
 import LogOut from './Components/Logout';
 import CreatePotluck from './Components/CreatePotluck';
@@ -12,16 +13,25 @@ import ViewEvents from './Components/ViewEvents'
 import Login from './Components/Login';
 
 // Routing
+
 import { Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+// Styling Imports
+
+import styled from 'styled-components';
 
 
 function App() {
+
+
+
   return (
-    <div className="App">
+    <Router>
       <header className="App-header">
         <h1>Potluck Planner</h1>
       </header>
-    </div>
+    </Router>
   );
 }
 

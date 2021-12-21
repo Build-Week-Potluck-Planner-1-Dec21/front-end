59cf25afd3c95c759b297a58ba164f627ce3b218
Peter Conley <peterdavidconley@gmail.com>
2021-12-21 05:31:57 -0500

    made imports into CreateAccount.js, created credentials slice of state, and handlers.

--- src/Components/CreateAccount.js
+++ src/Components/CreateAccount.js
@@ -1,10 +1,27 @@
 // Imports here
-import React from 'react'
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
+const initialCredentials = {
+    username: '',
+    password: '',
+}
 
 //Component
 
 const CreateAccount = () => {
+
+    const [ credentials, setCredentials ] = useState(initialCredentials);
+    const { push } = useHistory();
+
+    const handleChange = () => {
+
+    }
+
+    const handleSubmit = () => {
+        
+    }
 
     return(
         <div>

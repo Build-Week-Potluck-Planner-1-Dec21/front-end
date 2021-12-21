db8573cc0f9e358fcc0a144a92d1fecd18758079
Peter Conley <peterdavidconley@gmail.com>
2021-12-21 14:13:15 -0500

    made imports, created initial empty data, made single slice of state, and wrote handle functions

--- src/Components/EventRSVP.js
+++ src/Components/EventRSVP.js
@@ -1,9 +1,26 @@
 // Imports here
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
+// Initial data
+
+const initialEvent = {}
 
 //Component
 
 const EventRSVP = () => {
+
+    const [ event, setEvent ] = useState(initialEvent);
+    const { push } = useHistory();
+
+    const onChange = () => {
+
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+    }
 
     return(
         <div>

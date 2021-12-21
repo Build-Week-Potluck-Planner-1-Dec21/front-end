fdd6fef0241beeb78b191ea22db93194e0d1a79b
Peter Conley <peterdavidconley@gmail.com>
2021-12-21 14:33:01 -0500

    added form functionality

--- src/Components/EventRSVP.js
+++ src/Components/EventRSVP.js
@@ -14,30 +14,47 @@ const EventRSVP = () => {
     const [ event, setEvent ] = useState(initialEvent);
     const { push } = useHistory();
 
-    const onChange = () => {
+    const handleChange = () => {
 
     }
 
-    const onSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
     }
 
     return(
         <div>
+            <h2>RSVP This Event</h2>
             <form>
+                <center>
                 <label>Name:
                     <input />
                 </label>
+                <br />
+                <h4>What are you bringing?</h4>
                 <label>Item 1:
-                    <input type='text'/>
+                    <input 
+                    type='text'
+                    />
                 </label>
+                <br />
                 <label>Item 2:
-                    <input type='text'/>
+                    <input 
+                    type='text'
+                    placeholder='optional'
+                    />
                 </label>
+                <br />
                 <label>Item 3:
-                    <input type='text'/>
+                    <input 
+                    type='text'
+                    placeholder='optional'
+                    />
                 </label>
+                <br />
+                <br />
                 <button>Submit</button>
+                </center>
             </form>
 
         </div>

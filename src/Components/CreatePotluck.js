a52cee57b31e96ab0e1845ac1ae29aada3416ee1
jessica <fuerte.jessica10@gmail.com>
2021-12-21 12:28:06 -0600

    did createpotluck.js just need to add styling

--- src/Components/CreatePotluck.js
+++ src/Components/CreatePotluck.js
@@ -7,7 +7,51 @@ const CreatePotluck = () => {
 
     return(
         <div>
-
+          <h1>Create your Potluck!</h1>
+           
+            
+                <label>Nickname:
+                    <input 
+                    type="text" 
+                    name="nickname" 
+                    id="nickname"
+                    
+                    />
+                </label>
+                <label>Event Date:
+                    <input 
+                    type="text" 
+                    name="date" 
+                    id="date"
+                    
+                    />
+                </label>
+                <label>Location:
+                    <input 
+                    type="text" 
+                    name="location" 
+                    id="location"
+                    
+                    />
+                </label>
+                <label>Time:
+                    <input 
+                    type="text" 
+                    name="time" 
+                    id="time"
+                    
+                    />
+                </label>
+                <label>Public Event?:
+                    <input 
+                    type="checkbox" 
+                    name="public-event" 
+                    id="public-event"
+                    
+                    />
+                </label>
+                <button id='submit' >Submit</button>
+                
         </div>
     )
 };

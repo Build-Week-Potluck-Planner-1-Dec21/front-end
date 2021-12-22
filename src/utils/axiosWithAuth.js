e7bad06f40adca241e104ebade3773fb01648aa4
Peter Conley <peterdavidconley@gmail.com>
2021-12-22 11:56:32 -0500

    brian is helping with axiosAuth and handle functions in CreatePotluck

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -2,9 +2,13 @@ import axios from 'axios';
 
 const axiosWithAuth = () => {
     const token = localStorage.getItem('token');
+    const user_id = localStorage.getItem('user_id')
 
     return axios.create({
-        headers: { authorization: token },
+        headers: { 
+            authorization: token,
+            user_id: user_id,
+        },
         baseURL: 'https://potluckplan.herokuapp.com/api'
     })
 }

db522c2aafcd04df968be6bf8833888cda8adf0e
jessica <fuerte.jessica10@gmail.com>
2021-12-22 13:10:37 -0600

    added styling to nav

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,6 +3,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
 
+// Styling
+
+const NavStyling = {
+   display: 'flex',
+   flexDirection: 'row',
+   justifyContent: 'space-between',
+   textAlign: 'center',
+   backgroundColor: '#9b8da1',
+}
+
+const LinkStyling = {
+   color: 'white',
+   backgroundColor: '#9b8da1',
+   textAlign: 'center',
+   fontSize: 20,
+   padding: 15,
+}
+
+
+
 
 //Component
 
@@ -18,15 +38,15 @@ const NavBar = () => {
 
     return(
         
-        <div>
+        <div style ={NavStyling}>
             <Link to='/'><h2>Potluck Planner</h2></Link>
-            <Link to='/events'>View Potlucks</Link>
+            <Link to='/events' style={LinkStyling}>View Potlucks</Link>
             <br />
-            <Link to='/create-potluck'>Create Potluck</Link>
+            <Link to='/create-potluck' style={LinkStyling}>Create Potluck</Link>
             <br />
-            <Link to='/login'>Login</Link>
+            <Link to='/login' style={LinkStyling}>Login</Link>
             <br />
-            <button onClick={handleLogout}>Logout</button>
+            <button onClick={handleLogout} style={LinkStyling}>Logout</button>
             <i className="fas fa-sign-in-alt"></i>
         </div>
         

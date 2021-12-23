e958c455a167647816c5a24449b8b5006bd6bf3f
Claudia Scarbrough <rubyharrts@gmail.com>
2021-12-22 22:52:13 -0600

    home component styled

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
 // Imports here
 import React from "react";
+import { Link } from "react-router-dom";
 import "../Design Assets/assets/css/Home.css";
 
 //Component
@@ -9,6 +10,19 @@ const Home = () => {
     <div>
       <h1>Home Component</h1>
       <div className="home-img"></div>
+      <p>
+        Cheese triangles cream cheese edam. Cottage cheese airedale stinking
+        bishop cut the cheese parmesan squirty cheese cheese strings feta.
+        Monterey jack hard cheese bocconcini airedale camembert de normandie
+        taleggio parmesan red leicester. Dolcelatte macaroni cheese cheese
+        strings.
+      </p>
+      <Link to="/login" className="button-hm">
+        Login
+      </Link>
+      <Link to="/create-account" className="button-hm">
+        Click here.
+      </Link>
     </div>
   );
 };

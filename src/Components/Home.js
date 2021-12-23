e355c54ede7c13b6d198c95bc5a4e718d329e828
Peter Conley <peterdavidconley@gmail.com>
2021-12-23 06:00:52 -0500

    added edits to Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,7 @@
 // Imports here
 import React from "react";
 import "../DesignAssets/assets/css/Home.css";
+import { Link } from 'react-router-dom';
 
 //Component
 
@@ -9,6 +10,16 @@ const Home = () => {
     <div>
       <h1>Home Component</h1>
       <div className="home-img"></div>
+      <br></br>
+      <center>
+      <Link to="/login" className="button-hm">
+        Login
+      </Link>
+      <br></br>
+      <Link to="/create-account" className="button-hm">
+        Create Account
+      </Link>
+      </center>
     </div>
   );
 };

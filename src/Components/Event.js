7c2062e3da3f5f08d0a6a1c61c91cd43f38b78da
Peter Conley <peterdavidconley@gmail.com>
2021-12-23 05:24:48 -0500

    delete button function is properly working

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -23,7 +23,7 @@ const Event = (props) => {
 		axiosWithAuth()
 		.delete(`/potlucks/${event.potluck_id}`)
 		.then( resp => {
-			console.log(resp)
+			push(`/`)
 		})
 		.catch( err => {
 			console.log(err)
